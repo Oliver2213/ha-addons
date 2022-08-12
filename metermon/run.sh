950ed27b5d1021945e5a63c8545bc0f0240aacb3
Blake Oliver <oliver22213@me.com>
2022-08-12 17:50:51 -0400

    Fix typos in var names

--- metermon/run.sh
+++ metermon/run.sh
@@ -3,9 +3,9 @@
 RTL_TCP_SERVER=$(bashio::config 'rtl_address')
 RTL_TCP_PORT=$(bashio::config 'rtl_port')
 MQTT_BROKER_HOST=$(bashio::config 'mqtt_address')
-MQTT_BROKER_POERT=$(bashio::config 'rtl_mqtt_port')
+MQTT_BROKER_PORT=$(bashio::config 'mqtt_port')
 MQTT_USERNAME=$(bashio::config 'mqtt_username')
 MQTT_PASSWORD=$(bashio::config 'mqtt_password')
-send_raw=$(bashio::config 'send_raw')
+METERMON_SEND_RAW=$(bashio::config 'send_raw')
 
-python -u metermon.py
+python3 -u metermon.py
