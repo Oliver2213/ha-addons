7c69a16a61752a088922a7d34c15c89bbde6706e
Blake Oliver <oliver22213@me.com>
2022-08-12 17:16:05 -0400

    Metermon: add run.sh, which properly sets env vars for metermon and starts it; add more config options

---
+++ metermon/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/with-contenv bashio
+
+RTL_TCP_SERVER=$(bashio::config 'rtl_address')
+RTL_TCP_PORT=$(bashio::config 'rtl_port')
+MQTT_BROKER_HOST=$(bashio::config 'mqtt_address')
+MQTT_BROKER_POERT=$(bashio::config 'rtl_mqtt_port')
+MQTT_USERNAME=$(bashio::config 'mqtt_username')
+MQTT_PASSWORD=$(bashio::config 'mqtt_password')
+send_raw=$(bashio::config 'send_raw')
+
+bashio::log.info "$(./rtlamr --version)"
+bashio::log.info "Starting metermon..."
+python -u metermon.py
