a4cf9667b252485d79039013a367f3a911b7f934
Blake Oliver <oliver22213@me.com>
2022-08-12 18:26:05 -0400

    Log the mqtt username and password for debugging

--- metermon/run.sh
+++ metermon/run.sh
@@ -7,5 +7,5 @@ MQTT_BROKER_PORT=$(bashio::config 'mqtt_port')
 MQTT_USERNAME=$(bashio::config 'mqtt_username')
 MQTT_PASSWORD=$(bashio::config 'mqtt_password')
 METERMON_SEND_RAW=$(bashio::config 'send_raw')
-
+bashio::log.info "Connecting to broker with user $(MQTT_USERNAME) and password $(MQTT_PASSWORD)"
 python3 -u metermon.py
