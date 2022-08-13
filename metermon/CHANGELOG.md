8043a2da6b1e5fc5cbf411d175527cce5448938d
Blake Oliver <oliver22213@me.com>
2022-08-12 23:51:17 -0400

    Make metermon use homeassistant's mqtt service if no host is given

---
+++ metermon/CHANGELOG.md
@@ -0,0 +1,5 @@
+# 0.2.1
+* If no mqtt host is set, fall back to homeassistant's mqtt service info for host, port and credentials.
+
+# <= 0.2.0
+* Get the add-on functioning
--- metermon/run.sh
+++ metermon/run.sh
@@ -6,5 +6,17 @@ export MQTT_BROKER_PORT=$(bashio::config 'mqtt_port')
 export MQTT_USERNAME=$(bashio::config 'mqtt_username')
 export MQTT_PASSWORD=$(bashio::config 'mqtt_password')
 export METERMON_SEND_RAW=$(bashio::config 'send_raw')
-bashio::log.info "Connecting to broker with user $MQTT_USERNAME and password $MQTT_PASSWORD"
+
+# If no mqtt broker host, ask homeassistant
+# Note: the way this works means that if no host is set, the port, username and password options are ignored
+  # in favor of what homeassistant's mqtt service is set to.
+  # This is to allow for easy set-up in common cases.
+if [ -z "$MQTT_BROKER_HOST" ]; then
+  export MQTT_BROKER_HOST=$(bashio::services mqtt "host")
+  export MQTT_BROKER_PORT=$(bashio::services mqtt "port")
+  export MQTT_USERNAME=$(bashio::services mqtt "username")
+  export MQTT_PASSWORD=$(bashio::services mqtt "password")
+  bashio::log.info "No mqtt host set; connecting to homeassistant's mqtt service: $MQTT_USERNAME@$MQTT_BROKER_HOST:$MQTT_BROKER_PORT"
+fi
+#bashio::log.info "Connecting to broker with user $MQTT_USERNAME and password $MQTT_PASSWORD"
 python3 -u metermon.py
