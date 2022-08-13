8a9b5745ef01183a0726a7067eaec29b378bdef1
Blake Oliver <oliver22213@me.com>
2022-08-12 22:55:44 -0400

    Fix configuration: metermon now uses all values exposed through the UI

--- metermon/run.sh
+++ metermon/run.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
-RTL_TCP_SERVER=$(bashio::config 'rtl_address')
-RTL_TCP_PORT=$(bashio::config 'rtl_port')
-MQTT_BROKER_HOST=$(bashio::config 'mqtt_address')
-MQTT_BROKER_PORT=$(bashio::config 'mqtt_port')
-MQTT_USERNAME=$(bashio::config 'mqtt_username')
-MQTT_PASSWORD=$(bashio::config 'mqtt_password')
-METERMON_SEND_RAW=$(bashio::config 'send_raw')
-bashio::log.info "Connecting to broker with user $(MQTT_USERNAME) and password $(MQTT_PASSWORD)"
+export RTL_TCP_SERVER=$(bashio::config 'rtl_address')
+export MQTT_BROKER_HOST=$(bashio::config 'mqtt_address')
+export MQTT_BROKER_PORT=$(bashio::config 'mqtt_port')
+export MQTT_USERNAME=$(bashio::config 'mqtt_username')
+export MQTT_PASSWORD=$(bashio::config 'mqtt_password')
+export METERMON_SEND_RAW=$(bashio::config 'send_raw')
+bashio::log.info "Connecting to broker with user $MQTT_USERNAME and password $MQTT_PASSWORD"
 python3 -u metermon.py
